56fad5f17a8c85a5b6d5eacabb3e20d2bd124995
Sourav1429 <sourav.ganguly1429@gmail.com>
2022-08-31 01:26:45 +0530

    Updated file

--- direct_off_policy.py
+++ direct_off_policy.py
@@ -10,7 +10,7 @@ from find_target_policy import Target_Policy
 import numpy as np
 from Machine_replacement_env_create import Machine_Replacement
 from transition_matrix import beh_pol_sd
-T = 10 # number of times episode is run
+T = 500 # number of times episode is run
 nS = 4 #enter number of states
 nA = 2 #let this be 2 only
 rep_cost = 0.7 #replacement cost
@@ -25,27 +25,45 @@ l_rate=0.1
 isDeterministic = True;
 mr_obj = Machine_Replacement(rep_cost,nS,nA);
 P = mr_obj.gen_probability()
-R = mr_obj.gen_reward();
+R = mr_obj.gen_expected_reward();
 print("Probability matrix:");
 print(P);
 print("Reward matrix:");
 print(R);
 start = 0;
 tar_obj = Target_Policy(np.arange(nS), np.arange(nA), P, R, start)
-target_policy = tar_obj.find_optimum_policy(runs, T)[-1];
+#target_policy = tar_obj.find_optimum_policy(runs, T)[-1];
+target_policy = np.array([[0,0,0,1],
+                          [0,0,1,1],
+                          [0,1,1,1],
+                          [1,1,1,1]]);
 print("Target policy:");
 print(target_policy);
-
-sd_obj = average_case_state_dist(isDeterministic, T, nS, nA, behaviour_policy, target_policy, state, batch_size, l_rate, rep_cost)
-state_distribution_ratios,data = sd_obj.get_state_distribution();
-print("State distribution ratios:");
-print(state_distribution_ratios)
-
-
 beh_sd_obj = beh_pol_sd(P, behaviour_policy, nS, nA);
 behaviour_policy_state_distribution = beh_sd_obj.state_distribution_simulated(onehot_encode=0);
-print("Behaviour policy state distribution:");
-print(behaviour_policy_state_distribution);
-target_policy_distribution = state_distribution_ratios * behaviour_policy_state_distribution;
-target_policy_distribution = target_policy_distribution/np.sum(target_policy_distribution);
-print(len(data['reward']));
+for policy in target_policy:
+    sd_obj = average_case_state_dist(isDeterministic, T, nS, nA, behaviour_policy, policy, state, batch_size, l_rate, rep_cost)
+    state_distribution_ratios,data = sd_obj.get_state_distribution();
+    print("State distribution ratios:");
+    print(state_distribution_ratios)
+
+    print("Behaviour policy state distribution:");
+    print(behaviour_policy_state_distribution);
+    target_policy_distribution = state_distribution_ratios * behaviour_policy_state_distribution;
+    target_policy_distribution = target_policy_distribution/np.sum(target_policy_distribution);
+    
+    '''cost_dist = np.zeros(nS);
+    state_visitation = np.zeros(nS);
+    for i in range(T):
+        s = data['state'][i];
+        state_visitation[s]+=1;
+        cost_dist[s]+=R[target_policy[s],s];
+    print("Cost_distribution");
+    print(cost_dist/state_visitation);'''
+    print("Output_cost:");
+    cost = 0;
+    for i in range(nS):
+        cost = cost + target_policy_distribution[i]*R[policy[i],i];
+    print(cost);
+    
+    
